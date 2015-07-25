61f02578db935b48c488fc4a1885d63dd5a64556
RRR <840710343@qq.com>
2015-07-25 22:54:57 +0800

    Finish the addToMySQL

--- addToMySQL.php
+++ addToMySQL.php
@@ -5,23 +5,66 @@
 </head>
 <body>
 <?php
+
+test_input($_POST["id"]);
+test_input($_POST["name"]);
+if (strlen($_POST["id"]) != 8 || !is_numeric($_POST["id"]))
+    exit("提交失败,请检查输入的学号!");
+
 $servername = "localhost";
 $username = "root";
 $password = "soeasy";
-//echo $_SERVER["PHP_SELF"] . "<br>";
-//echo $_SERVER["REQUEST_METHOD"];
+$dbname = "free_time";
+$items = array(
+    "id", "name",
+    "Mon_m1", "Mon_m2", "Mon_a1", "Mon_a2", "Mon_e",
+    "Tue_m1", "Tue_m2", "Tue_a1", "Tue_a2", "Tue_e", 
+    "Wed_m1", "Wed_m2", "Wed_a1", "Wed_a2", "Wed_e",
+    "Thur_m1", "Thur_m2", "Thur_a1", "Thur_a2", "Thur_e",
+    "Fri_m1", "Fri_m2", "Fri_a1", "Fri_a2", "Fri_e",
+    "Sat_m", "Sat_a", "Sat_e",
+    "Sun_m", "Sun_a", "Sun_e"
+);
+foreach ($items as $item) {
+    if ($_POST[$item] == NULL) $_POST[$item] = "0";
+}
+
+//construct the MySQL insert command $sql
+$sql = "insert into lib(";
+foreach ($items as $item) {
+    $sql .= $item;
+    if ($item != end($items)) $sql .= ", ";
+}
+$sql .= ") values(";
+foreach ($items as $item) {
+    if ($item == "id")
+        $sql .= "\"" . $_POST["id"] . "\"";
+    elseif ($item == "name")
+        $sql .= ", \"" . $_POST["name"] . "\"";
+    else $sql .= ", " . $_POST[$item];
+}
+$sql .= ");";
+//echo "$sql<br>";
+
+//executive the MySQL command
 try {
-	$conn = new PDO("mysql:host=$servername;dbname=freetime", $username, $password);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$conn->exec("insert into lib (name, id, 
-	Mon_m1, Mon_m2, Mon_a1, Mon_a2, Mon_e, 
-	Tue_m1, Tue_m2, Tue_a1, Tue_a2, Tue_e,
-	Wed_m1, Wed_m2, Wed_a1, Wed_a2, Wed_e,
-	Thu_m1, Thu_m2, Thu_a1, Thu_a2, Thu_e,
-	Fri_m1, Fri_m2, Fri_a1, Fri_a2, Fri_e,
-	Sat_m, Sat_a, Sat_e,
-	Sun_m, Sun_a, Sun_e)
-	values (	
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("set names utf8;");//set the MySQL connecting encoding
+    $conn->exec($sql);
+    echo "提交成功!";
+} 
+catch (PDOException $e) {
+    echo "提交失败!<br>";
+    echo $e->getMessage();
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -27,55 +27,55 @@
 
 <tr>
 <th>上午（8:00-10:00）</th>
-<td><input type="checkbox" name="w1" value="m1"></td>
-<td><input type="checkbox" name="w2" value="m1"></td>
-<td><input type="checkbox" name="w3" value="m1"></td>
-<td><input type="checkbox" name="w4" value="m1"></td>
-<td><input type="checkbox" name="w5" value="m1"></td>
-<td><input type="checkbox" name="w6" value="m1"></td>
-<td rowspan="2"><input type="checkbox" name="w7" value="m1"></td>
+<td><input type="checkbox" name="Mon_m1" value="1"></td>
+<td><input type="checkbox" name="Tue_m1" value="1"></td>
+<td><input type="checkbox" name="Wed_m1" value="1"></td>
+<td><input type="checkbox" name="Thur_m1" value="1"></td>
+<td><input type="checkbox" name="Fri_m1" value="1"></td>
+<td><input type="checkbox" name="Sat_m1" value="1"></td>
+<td rowspan="2"><input type="checkbox" name="Sun_m1" value="1"></td>
 </tr>
 
 <tr>
 <th>上午（10:00-12:00）</th>
-<td><input type="checkbox" name="w1" value="m2"></td>
-<td><input type="checkbox" name="w2" value="m2"></td>
-<td><input type="checkbox" name="w3" value="m2"></td>
-<td><input type="checkbox" name="w4" value="m2"></td>
-<td><input type="checkbox" name="w5" value="m2"></td>
-<td><input type="checkbox" name="w6" value="m2"></td>
+<td><input type="checkbox" name="Mon_m2" value="1"></td>
+<td><input type="checkbox" name="Tue_m2" value="1"></td>
+<td><input type="checkbox" name="Wed_m2" value="1"></td>
+<td><input type="checkbox" name="Thur_m2" value="1"></td>
+<td><input type="checkbox" name="Fri_m2" value="1"></td>
+<td><input type="checkbox" name="Sat_m2" value="1"></td>
 </tr>
 
 <tr>
 <th>下午（13:00-15:00）</th>
-<td><input type="checkbox" name="w1" value="a1"></td>
-<td><input type="checkbox" name="w2" value="a1"></td>
-<td><input type="checkbox" name="w3" value="a1"></td>
-<td><input type="checkbox" name="w4" value="a1"></td>
-<td><input type="checkbox" name="w5" value="a1"></td>
-<td><input type="checkbox" name="w6" value="a1"></td>
-<td rowspan="2"><input type="checkbox" name="w7" value="a1"></td>
+<td><input type="checkbox" name="Mon_a1" value="1"></td>
+<td><input type="checkbox" name="Tue_a1" value="1"></td>
+<td><input type="checkbox" name="Wed_a1" value="1"></td>
+<td><input type="checkbox" name="Thur_a1" value="1"></td>
+<td><input type="checkbox" name="Fri_a1" value="1"></td>
+<td><input type="checkbox" name="Sat_a1" value="1"></td>
+<td rowspan="2"><input type="checkbox" name="Sun_a1" value="1"></td>
 </tr>
 
 <tr>
 <th>下午（15:00-17:00）</th>
-<td><input type="checkbox" name="w1" value="a2"></td>
-<td><input type="checkbox" name="w2" value="a2"></td>
-<td><input type="checkbox" name="w3" value="a2"></td>
-<td><input type="checkbox" name="w4" value="a2"></td>
-<td><input type="checkbox" name="w5" value="a2"></td>
-<td><input type="checkbox" name="w6" value="a2"></td>
+<td><input type="checkbox" name="Mon_a2" value="1"></td>
+<td><input type="checkbox" name="Tue_a2" value="1"></td>
+<td><input type="checkbox" name="Wed_a2" value="1"></td>
+<td><input type="checkbox" name="Thur_a2" value="1"></td>
+<td><input type="checkbox" name="Fri_a2" value="1"></td>
+<td><input type="checkbox" name="Sat_a2" value="1"></td>
 </tr>
 
 <tr>
 <th>晚上（18:00-22:00）</th>
-<td><input type="checkbox" name="w1" value="e"></td>
-<td><input type="checkbox" name="w2" value="e"></td>
-<td><input type="checkbox" name="w3" value="e"></td>
-<td><input type="checkbox" name="w4" value="e"></td>
-<td><input type="checkbox" name="w5" value="e"></td>
-<td><input type="checkbox" name="w6" value="e"></td>
-<td><input type="checkbox" name="w7" value="e"></td>
+<td><input type="checkbox" name="Mon_e" value="1"></td>
+<td><input type="checkbox" name="Tue_e" value="1"></td>
+<td><input type="checkbox" name="Wed_e" value="1"></td>
+<td><input type="checkbox" name="Thur_e" value="1"></td>
+<td><input type="checkbox" name="Fri_e" value="1"></td>
+<td><input type="checkbox" name="Sat_e" value="1"></td>
+<td><input type="checkbox" name="Sun_e" value="1"></td>
 </tr>
 </table>
 
